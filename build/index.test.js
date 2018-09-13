0efd3e2a1638358ee3f1f63a3654ea91ea9bf4c9
luke0b1000 <luke0b1000@gmail.com>
2018-09-13 15:02:45 -0400

    Fixed *****

--- build/index.test.js
+++ build/index.test.js
@@ -2,10 +2,6 @@
 
 var _ = require(".");
 
-var _2 = _interopRequireDefault(_);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-test("This test replaces blacklisted words with asterisks***", function () {
-    return expect((0, _2.default)("The name of the NX will be the Nintendo Switch. The switch will be awesome.", ["switch"])).toBe("The name of the NX will be the Nintendo ***. The *** will be awesome.");
+test("This test replaces blacklisted words with asterisks*****", function () {
+    return expect((0, _.sensitiveWords)("The name of the NX will be the Nintendo Switch. The switch will be awesome.", ["switch"])).toBe("The name of the NX will be the Nintendo *****. The ***** will be awesome.");
 });
--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,11 @@
-import sensitiveWords from ".";
+import { sensitiveWords } from ".";
 
-test("This test replaces blacklisted words with asterisks***", () =>
+test("This test replaces blacklisted words with asterisks*****", () =>
     expect(
-        sensitiveWords("The name of the NX will be the Nintendo Switch. The switch will be awesome.", [
-            "switch"
-        ])
-    ).toBe("The name of the NX will be the Nintendo ***. The *** will be awesome."));
+        sensitiveWords(
+            "The name of the NX will be the Nintendo Switch. The switch will be awesome.",
+            ["switch"]
+        )
+    ).toBe(
+        "The name of the NX will be the Nintendo *****. The ***** will be awesome."
+    ));
